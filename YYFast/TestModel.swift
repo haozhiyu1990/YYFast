26599fc27acaebe4746e6a6bbefba5b3f6e8e72d
郝志宇 <hzy901110@gmail.com>
2019-01-15 09:46:09 +0800

    ...

---
+++ YYFast/TestModel.swift
@@ -0,0 +1,13 @@
+//
+//  TestModel.swift
+//  YYFastDemo
+//
+//  Created by haozhiyu on 2019/1/15.
+//  Copyright © 2019 haozhiyu. All rights reserved.
+//
+
+import UIKit
+
+class TestModel: NSObject {
+
+}
